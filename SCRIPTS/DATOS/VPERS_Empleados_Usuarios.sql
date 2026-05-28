d180be92cddb718abc0b86b454b48ea23e0326c7
EUSKODATA\aitor <aitor@euskodata.com>
2026-05-28 13:52:45 +0200

    Cambios:   - Añadidos scripts sql

--- SCRIPTS/DATOS/VPERS_Empleados_Usuarios.sql
+++
@@ -1,8 +0,0 @@
-CREATE   OR ALTER  VIEW [dbo].[VPERS_Empleados_Usuarios] AS  
-SELECT  E.FullName, isnull(Email, E.Mail) AS Email, A.Id, E.EmployeeId, A.UserName  
-FROM Employees E   
-JOIN [SEBASTIAN_HR_PROGECO_IC].dbo.AspNetUsers A  
-ON E.EmployeeId = A.Reference  
-WHERE A.OriginId =2
-AND LEN(Email) > 0
-GO
